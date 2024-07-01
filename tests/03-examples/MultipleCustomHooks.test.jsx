4c610912d8926afdf6f31cc47a654c2c49f69f05
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-07-01 19:45:49 +0200

    Testing MultipleCustomHooks

---
+++ tests/03-examples/MultipleCustomHooks.test.jsx
@@ -0,0 +1,13 @@
+
+
+describe('Pruebas en <MultipleCustomHooks />', () => {
+
+  test('Debe de mostrar el componente por defecto', () => {
+    
+  });
+
+  // test('Debe de mostrar el componente por defecto', () => {
+
+  // });
+
+});
