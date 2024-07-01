36f735fb62c866b536a5b737100ffbab87bb6d22
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-07-01 17:33:41 +0200

    useCounter test completed all ok

--- tests/hooks/useCounter.test.js
+++ tests/hooks/useCounter.test.js
@@ -1,5 +1,6 @@
 const { renderHook } = require("@testing-library/react");
 const { useCounter } = require("../../src/hooks/useCounter");
+const { act } = require("react");
 
 
 describe('Pruebas en el useCounter', () => {
@@ -18,8 +19,48 @@ describe('Pruebas en el useCounter', () => {
   test('Debe de generar el counter con el valor de 100', () => {
     const { result } = renderHook( () => useCounter(100) );
     const { counter } = result.current;
-    
+
     expect( counter ).toBe( 100 );
+
+  });
+
+  test('Debe de incrementar el contador', () => {
+    const { result } = renderHook( () => useCounter(100) );
+    const { counter, increment } = result.current;
+
+    act( () => {
+      increment();
+      increment(2);
+    });
+
+    expect( result.current.counter ).toBe( 103 );
+
+  });
+
+  test('Debe de decrementar el contador', () => {
+    const { result } = renderHook( () => useCounter(100) );
+    const { counter, decrement } = result.current;
+
+    act( () => {
+      decrement();
+      decrement(2);
+    });
+
+    expect( result.current.counter ).toBe( 97 );
+
+  });
+
+  test('Debe de realizar el reset', () => {
+    const { result } = renderHook( () => useCounter(100) );
+    const { counter, increment, reset } = result.current;
+
+    act( () => {
+      increment();
+      increment(2);
+      reset()
+    });
+
+    expect( result.current.counter ).toBe( 100 );
 
   });
 
